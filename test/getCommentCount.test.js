c2c5305e9f8f059559273a64f84390f4a5d0d3f4
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-05-23 22:42:23 +0500

    fixing linter

--- test/getCommentCount.test.js
+++ test/getCommentCount.test.js
@@ -1,4 +1,4 @@
-import getCommentCount from '../src/modules/getCommentCount';
+import getCommentCount from '../src/modules/getCommentCount.js';
 
 describe('Comment Count', () => {
   test('Output should be ', () => {
@@ -6,9 +6,8 @@ describe('Comment Count', () => {
     const output = getCommentCount(comments);
     expect(output).toBe(0);
   });
-  
   test('Output should be 1', () => {
-    const comments = [{id: '1'}];
+    const comments = [{ id: '1' }];
     const output = getCommentCount(comments);
 
     expect(output).toBe(1);
