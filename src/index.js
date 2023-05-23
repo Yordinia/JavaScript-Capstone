977f698087a32df5f3ffd5f06184584774328535
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-05-23 16:58:48 +0500

    adding modal for details and comments

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './style.css';
+import modal from './modules/modal-block.js';
+
+document.addEventListener('click', (event) => {
+  const comments = document.querySelectorAll('.comments');
+  const close = document.querySelector('.fa-xmark');
+  if (event.target === close) {
+    document.querySelector('.modal').classList.remove('view');
+  }
+  comments.forEach((div,index) => {
+    if(event.target === div) {
+      document.querySelector('.modal').classList.add('view');
+      modal(index);
+    }
+  });
+});
+
+const form = document.querySelector('#comment-form');
+if (form !== null){
+  
+  form.addEventListener('submit', (e) => {
+    console.log('here');
+    e.preventDefault();
+  });
+}
