6cee0926f3f3cce572f62e729b10637e559168a2
Yordi <yorditjordin@gmail.com>
2023-05-25 21:15:26 +0300

    test

--- test/testCount.test.js
+++ test/testCount.test.js
@@ -1,8 +1,34 @@
-console.log('running test')
+// import counter from '../src/modules/counter.js';
+const { JSDOM } = require('jsdom');
+const { experiments } = require('webpack');
 
-describe('Count test', ()=>{
-    
-})
+const dom = new JSDOM('<!doctype html><html><body> this is <button> in a button </button> text</body></html>');
+global.document = dom.window.document;
+global.window = dom.window;
+global.navigator = dom.window.navigator;
+
+const hr = () => {
+  console.log('````````````````````````````````````````');
+};
+let x = 0;
+console.log('running test', JSDOM, x);
+
+describe('Count Test', () => {
+  test('count ', () => {
+    x += 1;
+    //console.log('this is dom', dom);
+    hr();
+    // console.log('this is dom window', dom.window);
+    // hr()
+    // console.log('this is dom window navigator', dom.window.navigator);
+    // hr()
+    console.log('this is dom.window.document', dom.window.document);
+    //hr();
+    console.log('this is dom.window.document.body.textcont', dom.window.document.body.textContent);
+    hr();
+    // expect(x).toBe(1)
+  });
+});
 
 // const testA = () => {
 // console.log('this is test A')
