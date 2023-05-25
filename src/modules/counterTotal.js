976702c7a5e57f64696d1bd0e294f94e0705f7ba
Yordi <yorditjordin@gmail.com>
2023-05-25 23:59:23 +0300

    Update counterTotal.js

--- src/modules/counterTotal.js
+++ src/modules/counterTotal.js
@@ -1,8 +1,8 @@
-const counterTotal = ( meals) => {
+const counterTotal = (meals) => {
   const className = '.card';
   // like number from DOM
   const totalMeals = document.querySelectorAll(className);
-  let flag = (totalMeals.length === meals.length)
+  const flag = (totalMeals.length === meals.length);
   return flag;
 };
 export default counterTotal;
