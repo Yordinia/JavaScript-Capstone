4e78956726fe7d20ce8d462319732eec9007491f
Yordi <yorditjordin@gmail.com>
2023-05-25 20:32:08 +0300

    install jsdon

--- test/testCount.test.js
+++ test/testCount.test.js
@@ -1,5 +1,22 @@
 console.log('running test')
 
-describe ('Testing Like Count'){
+describe('Count test', ()=>{
     
-}
+})
+
+// const testA = () => {
+// console.log('this is test A')
+// return true;
+// }
+
+// const testB = () => {
+// console.log('this is test B')
+// return true;
+
+// }
+// const desc = () => {
+//     testA();
+//     testB();
+// }
+
+// describe ('Testing Like Count',desc);
