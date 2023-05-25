23e44be2e99636fd1a12494160dd90dc6f07154a
Yordi <yorditjordin@gmail.com>
2023-05-25 15:44:20 +0300

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
 import './style.css';
 
-/* These are the tasks for this branch
-
-When the page loads, the webapp retrieves data from the selected API and shows the list of items on screen with the basic data (e.g. title + image).
-
-/Prepare also "Comments" and "Reservations" buttons. They should be doing nothing - just being displayed.
-
-This task does not include displaying the number of likes for each item.
-
-This task does not include the counter of the items. */
-
-    
 // Function to make API request and create cards
 async function fetchMeals() {
   const meals = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken';
   const response = await fetch(meals);
   const data = await response.json();
-  const first9Meals = data.meals.filter((m,i)=> i<9);
+  const first9Meals = data.meals.filter((m, i) => i < 9);
   return first9Meals;
 }
 
-const getCard = meal => {
+const getCard = (meal) => {
   const card = `
   <div class="card border-primary mb-3" style="max-width: 20rem;">
     <div class="card-header" id="header">
@@ -44,21 +33,20 @@ const getCard = meal => {
         <button type="button" class="btn btn-dark">Reservations</button>
       </div>
     </div>
-  </div>`
+  </div>`;
   return card;
-}
+};
 
 // display all 9 random meals
-const screenMeals = meals => {
-const mealElement = document.getElementById('homepage');
+const screenMeals = (meals) => {
+  const mealElement = document.getElementById('homepage');
 
-meals.forEach(meal => {
-  const card = getCard(meal);
-  mealElement.insertAdjacentHTML("beforeend", card);
+  meals.forEach((meal) => {
+    const card = getCard(meal);
+    mealElement.insertAdjacentHTML('beforeend', card);
   });
-}
+};
 
 // Call the function to fetch and display random meals
 
 screenMeals(await fetchMeals());
-
