fe42e12598dd6195cbb766dd5ed1b7ac5c68f690
Yordi <yorditjordin@gmail.com>
2023-05-25 13:04:02 +0300

    Like successful

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@ import './style.css';
 import getLikes from './modules/get_number_of_likes';
 import setLikes from './modules/set_number_of_likes';
 
+//here
+document.addEventListener('click', (event) => {
+  const buttons = document.querySelectorAll('.like-buttons');
+  let flag = false;
+
+  buttons.forEach((button) => {
+    let id;
+    if (button.contains(event.target)) {
+      flag = true;
+      id = button.id;
+      return;
+    }
+  });
+  if(flag){
+      console.log('button.id',flag,id)
+     // setLikes(id)
+    }
+});
+
 // Function to make API request and create cards
 async function fetchMeals() {
   const mealsURL = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken';
@@ -10,10 +29,8 @@ async function fetchMeals() {
   return meals;
 }
 
-const getCard = async (meal) => {
-  // get likes
-  const likes = await getLikes(meal);
-
+const getCard = (meal, {likes}) => {
+  
   const card = `
   <div class="card border-primary mb-3" style="max-width: 20rem;">
     <div class="card-header" id="header">
@@ -21,12 +38,9 @@ const getCard = async (meal) => {
     <div class= 'title-like-container'>  
         <h1>${meal.strMeal} </h1>
         <div class='heart'> 
-          <button class='btn btn-success' onclick= 'setLikes(${{id: meal.idMeal, likes}})'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="pink" class="bi bi-heart" viewBox="0 0 16 16">
-            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
-            </svg>
+          <button class='btn btn-success like-buttons' id='${meal.idMeal}' >
+            LIKE - ${likes}
           </button>
-          <p class='no-of-likes' id=${meal.idMeal} > likes </p>  
         </div>  
     </div>
     </div>
@@ -38,18 +52,26 @@ const getCard = async (meal) => {
       </div>
     </div>
   </div>`;
+
   return card;
 };
 
 // display all 9 random meals
-const screenMeals = (meals) => {
+const screenMeals = ({meals, likes}) => {
+  let like;
   const mealElement = document.getElementById('homepage');
-  meals.forEach(async(meal) => {
-    const card = await getCard(meal);
-    mealElement.insertAdjacentHTML('beforeend', card);
+  meals.forEach((meal) => {
+    like = likes.find(like=> like.item_id === meal.idMeal) || {idMeal:meal.idMeal, likes: 0};
+    console.log(like)
+    const card = getCard(meal, like);
+    mealElement.insertAdjacentHTML('beforeend', card);    
   });
+  
+  
 };
 
 // Call the function to fetch and display random meals
 const meals =  await fetchMeals();
-screenMeals(meals);
+const likes = await getLikes();
+
+screenMeals({meals, likes});
--- src/modules/set_number_of_likes.js
+++ src/modules/set_number_of_likes.js
@@ -1,18 +1,16 @@
-// const setLikes = ({id, likes}) => {
-//     const apiUrl = "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/";
-//     const idUrl = "9vUKLfgfPbeVlsgu5dzp"; // I didn't use our id vKZCdN5l6d0ODFZiWaxR so that I don't mess up the values (like I did with this one)
+const setLikes = async(id) => {
+    const apiUrl = "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/";
+    const idUrl = "9vUKLfgfPbeVlsgu5dzp"; // I didn't use our id vKZCdN5l6d0ODFZiWaxR so that I don't mess up the values (like I did with this one)
     
-//     fetch(`${apiUrl}${idUrl}/likes/`, {
-//       method: 'POST',
-//       headers: {
-//         'Content-type': 'application/json',
-//       },
-//       body: JSON.stringify({ item_id: id, likes: like })
-//     });
-// console.log('this is set Likes', id, likes)
-// }
-const setLikes = ({id, likes}) => {
-console.log('this is set likes', id , likes)
+    // fetch(`${apiUrl}${idUrl}/likes/`, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-type': 'application/json',
+    //   },
+    //   body: JSON.stringify({ idMeal: id })
+    // });
+    const val =  await response.text();
+console.log('this is set Likes', id , val)
 }
 
 export default setLikes;
