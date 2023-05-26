798a68787a67f8b8218cd6143b3187456ee634da
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-05-26 13:39:14 +0500

    fixing linter

--- src/modules/get_number_of_likes.js
+++ src/modules/get_number_of_likes.js
@@ -2,15 +2,13 @@ const getLikes = async () => {
   const apiUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
   const idUrl = '9vUKLfgfPbeVlsgu5dzp';
 
-  const response = await fetch(`${apiUrl}${idUrl}/likes/`).catch((error) => console.log('Fetch Error', error));
+  const response = await fetch(`${apiUrl}${idUrl}/likes/`);
   let data;
   const isJson = (response.headers.get('Content-type') === 'application/json');
   if (isJson) {
     data = await response.json();
-    console.log('content-type==json? ', isJson, 'get response-', response, 'data- ', data);
   } else {
     data = JSON.parse(await response.text());
-    // console.log('content-type==json? ', isJson ,data, 'url', `${apiUrl}${idUrl}/likes/`);
   }
   return data;
 };
